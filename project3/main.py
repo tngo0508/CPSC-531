f5d5a2d18758c536322fd982ae5bf4ca45bb5fa6
Thomas Ngo <tngo0508@gmail.com>
2019-12-08 01:09:56 -0800

    fix secondary index implementation

--- project3/main.py
+++ project3/main.py
@@ -4,17 +4,13 @@ from collections import namedtuple
 from datetime import date
 import dbm.ndbm
 import sys
+import os
 import argparse
 
 
 def create_primary_index(file_name):
-    num_blocks = 0
-    if file_name == 'small.bin':
-        num_blocks = 10
-    elif file_name == 'large.bin':
-        num_blocks = 1048576
-    else:
-        raise ValueError('file_name has to be either small.bin or larger.bin')
+    file_size = os.stat(file_name).st_size
+    num_blocks = file_size // 4096
     with dbm.ndbm.open('index', 'n') as db:
         offset = 0
         for i in range(1, num_blocks + 1):
@@ -35,10 +31,10 @@ def table_scan(file_name, verbose):
 
                 start = 0
                 for _ in range(10):
-                    fname, lname, job, comp, addr, phone, day, month, year, ssn, uname, email, url = unpack(
-                    '20s20s70s40s80s25s3I12s25s50s50s', data[start:start+405])
+                    fname, lname, _, _, _, _, day, month, year, ssn, _, _, _ = unpack(
+                        '20s20s70s40s80s25s3I12s25s50s50s', data[start:start+405])
                     age = calculate_age(day, month, year)
-                    if  age < 21:
+                    if age < 21:
                         fname = fname.replace(b'\x00', b'')
                         lname = lname.replace(b'\x00', b'')
                         ssn = ssn.replace(b'\x00', b'')
@@ -68,7 +64,7 @@ def uniqueness_check(file_name, verbose):
 
                 start = 0
                 for _ in range(10):
-                    fname, _, _, _, _, _, _, _, _, ssn, _, _, _ = unpack(
+                    _, _, _, _, _, _, _, _, _, ssn, _, _, _ = unpack(
                         '20s20s70s40s80s25sIII12s25s50s50s', data[start:start+405])
                     ssn = ssn.replace(b'\x00', b'')
                     if ssn not in seen:
@@ -82,13 +78,8 @@ def uniqueness_check(file_name, verbose):
 
 
 def create_secondary_index(file_name):
-    num_blocks = 0
-    if file_name == 'small.bin':
-        num_blocks = 10
-    elif file_name == 'large.bin':
-        num_blocks = 1048576
-    else:
-        raise ValueError('file_name has to be either small.bin or larger.bin')
+    file_size = os.stat(file_name).st_size
+    num_blocks = file_size // 4096
     with dbm.ndbm.open('secondaryIndex', 'n') as secondaryIndex:
         with open(file_name, 'rb') as f:
             record_pointer = 0
@@ -99,14 +90,15 @@ def create_secondary_index(file_name):
                 start = 0
                 for _ in range(10):
                     _, _, _, _, _, _, day, month, year, _, _, _, _ = unpack(
-                    '20s20s70s40s80s25s3I12s25s50s50s', data[start:start+405])
+                        '20s20s70s40s80s25s3I12s25s50s50s', data[start:start+405])
 
                     birthdate = pack('3h', day, month, year)
 
                     if birthdate not in secondaryIndex:
                         secondaryIndex[birthdate] = pack('I', record_pointer)
                     else:
-                        new_record_pointer = secondaryIndex[birthdate] + pack('I', record_pointer)
+                        new_record_pointer = secondaryIndex[birthdate] + pack(
+                            'I', record_pointer)
                         secondaryIndex[birthdate] = new_record_pointer
                     start += 405
                     record_pointer += 405
@@ -124,12 +116,13 @@ def table_scan_on_secondary_index(file_name, verbose):
                 if age < 21:
                     num_record_pointers = len(secondaryIndex[birthdate]) // 4
                     format_char = str(num_record_pointers) + 'I'
-                    record_pointers = unpack(format_char, secondaryIndex[birthdate])
+                    record_pointers = unpack(
+                        format_char, secondaryIndex[birthdate])
                     for record_pointer in record_pointers:
                         f.seek(record_pointer)
                         data = f.read(405)
                         fname, lname, _, _, _, _, _, _, _, ssn, _, _, _ = unpack(
-                        '20s20s70s40s80s25sIII12s25s50s50s', data)
+                            '20s20s70s40s80s25sIII12s25s50s50s', data)
                         fname = fname.replace(b'\x00', b'')
                         lname = lname.replace(b'\x00', b'')
                         ssn = ssn.replace(b'\x00', b'')
@@ -139,34 +132,123 @@ def table_scan_on_secondary_index(file_name, verbose):
     print("Number of records: {}".format(count))
 
 
-def main(args):
-    if args.primary:
-        create_primary_index(args.file)
-    if args.secondary:
-        create_secondary_index(args.file)
-    if args.query1:
-        table_scan(args.file, args.verbose)
-    if args.query2:
-        uniqueness_check(args.file, args.verbose)
-    if args.query3:
-        table_scan_on_secondary_index(args.file, args.verbose)
+def generate_secondary_index_file(file_name):
+    file_size = os.stat(file_name).st_size
+    num_blocks = file_size // 4096
+    with open('secondary_index_file.bin', 'wb') as secondary_index_file:
+        with open(file_name, 'rb') as f:
+            record_pointer = 0
+            for _ in range(num_blocks):
+                record_pointer = f.tell()
+                data = f.read(4096)
+
+                start = 0
+                for _ in range(10):
+                    _, _, _, _, _, _, day, month, year, _, _, _, _ = unpack(
+                        '20s20s70s40s80s25s3I12s25s50s50s', data[start:start+405])
+
+                    index_entry = pack('4I', day, month, year, record_pointer)
+                    secondary_index_file.write(index_entry)
+
+                    start += 405
+                    record_pointer += 405
+
+
+def create_secondary_index2(file_name):
+    file_size = os.stat(file_name).st_size
+    num_blocks = file_size // 4096
+    with dbm.ndbm.open('secondaryIndex', 'n') as secondaryIndex:
+        with open(file_name, 'rb') as f:
+            record_pointer = 0
+            for _ in range(num_blocks):
+                record_pointer = f.tell()
+                data = f.read(4096)
+
+                start = 0
+                for _ in range(256):
+                    day, month, year, record_pointer = unpack(
+                        '4I', data[start:start+16])
+                    birthdate = pack('3I', day, month, year)
+
+                    if birthdate not in secondaryIndex:
+                        secondaryIndex[birthdate] = pack('I', record_pointer)
+                    else:
+                        new_record_pointer = secondaryIndex[birthdate] + pack(
+                            'I', record_pointer)
+                        secondaryIndex[birthdate] = new_record_pointer
+                    start += 16
+                    record_pointer += 16
+
+
+def table_scan_on_secondary_index2(file_name, verbose):
+    count = 0
+    Person = namedtuple(
+        'Person', 'fname, lname, ssn, age')
+    with dbm.ndbm.open('secondaryIndex', 'r') as secondaryIndex:
+        with open(file_name, 'rb') as f:
+            for birthdate in secondaryIndex.keys():
+                day, month, year = unpack('3I', birthdate)
+                age = calculate_age(day, month, year)
+                if age < 21:
+                    num_record_pointers = len(secondaryIndex[birthdate]) // 4
+                    format_char = str(num_record_pointers) + 'I'
+                    record_pointers = unpack(
+                        format_char, secondaryIndex[birthdate])
+                    for record_pointer in record_pointers:
+                        f.seek(record_pointer)
+                        data = f.read(405)
+                        fname, lname, _, _, _, _, _, _, _, ssn, _, _, _ = unpack(
+                            '20s20s70s40s80s25sIII12s25s50s50s', data)
+                        fname = fname.replace(b'\x00', b'')
+                        lname = lname.replace(b'\x00', b'')
+                        ssn = ssn.replace(b'\x00', b'')
+                        count += 1
+                        if verbose:
+                            print(Person(fname, lname, ssn, age))
+    print("Number of records: {}".format(count))
+
+
+def main(ARGS):
+    if ARGS.primary:
+        create_primary_index(ARGS.file)
+    if ARGS.secondary:
+        generate_secondary_index_file(ARGS.file)
+        create_secondary_index2('secondary_index_file.bin')
+    if ARGS.cluster:
+        pass
+    if ARGS.query1:
+        table_scan(ARGS.file, ARGS.verbose)
+    if ARGS.query2:
+        uniqueness_check(ARGS.file, ARGS.verbose)
+    if ARGS.query3:
+        table_scan_on_secondary_index2(ARGS.file, ARGS.verbose)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Fall 2019 - Advanced Database Project 3')
-    parser.add_argument('file', help='binary file (small.bin or large.bin)')
-    parser.add_argument('-v', '--verbose', help='Show output records', action='store_true')
+    PARSER = argparse.ArgumentParser(
+        description='Fall 2019 - Advanced Database Project 3')
+    PARSER.add_argument('file', help='binary file (small.bin or large.bin)')
+    PARSER.add_argument('-v', '--verbose',
+                        help='Show output records', action='store_true')
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-p', '--primary', help='Create auto increment index', action='store_true')
-    group.add_argument('-s', '--secondary', help='Create secondary index on birthdate', action='store_true')
-    group.add_argument('-q1', '--query1', help='Table scan', action='store_true')
-    group.add_argument('-q2', '--query2', help='Uniqueness check', action='store_true')
-    group.add_argument('-q3', '--query3', help='Secondary index', action='store_true')
+    GROUP = PARSER.add_mutually_exclusive_group()
+    GROUP.add_argument('-p', '--primary',
+                       help='Create auto increment index', action='store_true')
+    GROUP.add_argument(
+        '-s', '--secondary', help='Create secondary index on birthdate', action='store_true')
+    GROUP.add_argument(
+        '-c', '--cluster', help='Create cluster index on birthdate', action='store_true')
+    GROUP.add_argument('-q1', '--query1', help='Table scan',
+                       action='store_true')
+    GROUP.add_argument('-q2', '--query2',
+                       help='Uniqueness check', action='store_true')
+    GROUP.add_argument('-q3', '--query3',
+                       help='Secondary index', action='store_true')
 
-    args = parser.parse_args()
-    if not any([args.primary, args.secondary, args.query1, args.query2, args.query3, args.verbose]):
-        parser.print_help()
+    ARGS = PARSER.parse_args()
+    if not any([ARGS.primary, ARGS.secondary, ARGS.query1, ARGS.query2, ARGS.query3,
+                ARGS.verbose, ARGS.cluster]):
+        PARSER.print_help()
         print('\nOne of the options is required')
         sys.exit(0)
-    main(args)
+    main(ARGS)
